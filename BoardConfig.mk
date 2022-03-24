9c0d383e142fb5dd24e78c296bb5e2f73bb3b5fb
kleidione Freitas <kleidione@gmail.com>
2022-03-24 19:02:29 -0300

    veux: proprietary-files: Update veux vendor blobs
    - Extracted veux-user 12 SKQ1.211006.001 V13.0.4.0.SKCMIXM
    
    Signed-off-by: kleidione <kleidione@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,7 +203,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security Patch Level
-VENDOR_SECURITY_PATCH := 2022-07-01
+VENDOR_SECURITY_PATCH := 2022-10-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
--- libinit/init_xiaomi_veux.cpp
+++ libinit/init_xiaomi_veux.cpp
@@ -18,7 +18,7 @@ static const variant_info_t veux_global_info = {
     .marketname = "Redmi Note 11 Pro 5G",
     .model = "2201116SG",
     .name = "veux_global",
-    .build_fingerprint = "Redmi/veux_global/veux:12/SKQ1.211006.001/V13.0.2.0.SKCMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/veux_global/veux:12/SKQ1.211006.001/V13.0.4.0.SKCMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -84,7 +84,7 @@ static const variant_info_t veux_p_global_info = {
     .marketname = "POCO X4 Pro 5G",
     .model = "2201116PG",
     .name = "veux_p_global",
-    .build_fingerprint = "POCO/veux_p_global/veux:12/SKQ1.211006.001/V13.0.2.0.SKCMIXM:user/release-keys",
+    .build_fingerprint = "POCO/veux_p_global/veux:12/SKQ1.211006.001/V13.0.4.0.SKCMIXM:user/release-keys",
 
     .nfc = true,
 };
