8a3a43d20c76ec86e70276b32bb1c3e97ac0151a
kleidione <kleidione@gmail.com>
2022-03-29 18:59:26 -0300

    veux: Add Google Camera Go Prebuilt
    - CameraGo_2.5_Greatness_Special-2 (com.google.android.apps.cameralite)
    
    - Support Group: https://t.me/GcamGo
    
    Signed-off-by: kleidione <kleidione@gmail.com>

--- device.mk
+++ device.mk
@@ -110,6 +110,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    GoogleCameraGo \
     libcamera2ndk_vendor \
     libstdc++.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
