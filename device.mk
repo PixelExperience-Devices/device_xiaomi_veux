066a6f56e8ef17c26496fc9398ceb23c91e429d1
madmax7896 <madmax78968@gmail.com>
2022-03-29 08:05:06 -0300

    veux: Move msm_irqbalance.conf to dt

--- device.mk
+++ device.mk
@@ -363,6 +363,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
