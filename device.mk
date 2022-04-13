1fd6bbfea6e3391336137c5f342d7d11409470a5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-14 01:48:37 +0200

    veux: Move to common IFAAService
    Change-Id: I71bfdacc4626d75d56aa12ec384f0f116d6d6ff2
    Signed-off-by: Ahmad Rulim <personal@rulim34.dev>

--- device.mk
+++ device.mk
@@ -229,10 +229,7 @@ PRODUCT_COPY_FILES += \
 
 # IFAA Manager
 PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+    IFAAService
 
 # IPACM
 PRODUCT_PACKAGES += \
