4e2c4fab8a579b3af4d4193065fbb0d29e2e96ad
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:29:35 +0200

    veux: Stop explicitely building netutils-wrapper-1.0
    * Built by default
    
    Change-Id: Id6666535bb8d0b84d2d0a8d19411fafa49df3d68
    Signed-off-by: Ahmad Rulim <personal@rulim34.dev>

--- device.mk
+++ device.mk
@@ -269,9 +269,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
 
-PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0
-
 # Neural Networks
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
