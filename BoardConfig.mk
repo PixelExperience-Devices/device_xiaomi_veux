7bf112570aa7b5fab718dc587c000525e8684df9
kubersharma001 <kubersharma001@gmail.com>
2022-06-23 10:53:31 +0530

    veux: Import product props
     - V13.0.8.0.RKCMIXM
    
    Change-Id: I54c4a00971a42750a4b0809913250f72b0b7c167

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,6 +187,7 @@ TARGET_BOARD_PLATFORM := holi
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
