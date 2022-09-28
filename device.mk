0105dcda8d7070cb0ea7aa7ded21d141ea5b6a6d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-29 00:43:20 +0200

    veux: Remove non-existing or implicitly included IPACM packages
    Change-Id: Iae06f09e101b237187638e76a6dd903ecdc2ca4f
    Signed-off-by: Ahmad Rulim <personal@rulim34.dev>

--- device.mk
+++ device.mk
@@ -245,9 +245,7 @@ PRODUCT_BOOT_JARS += \
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
-    IPACM_cfg.xml \
-    libipanat \
-    liboffloadhal
+    IPACM_cfg.xml
 
 # Keymaster
 PRODUCT_PACKAGES += \
