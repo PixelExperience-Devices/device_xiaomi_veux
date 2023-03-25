fd718f1705a13e16391ecca65509f1b07bced839
Volodymyr Zhdanov <wight554@gmail.com>
2023-03-25 21:49:44 +0300

    veux: Disable configstore  * it's finally deprecated in R  * ship disable_configstore because we have API level 29
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>
    Signed-off-by: Ahmad Rulim <personal@rulim34.dev>

--- device.mk
+++ device.mk
@@ -132,6 +132,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
+# Disable SF configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
