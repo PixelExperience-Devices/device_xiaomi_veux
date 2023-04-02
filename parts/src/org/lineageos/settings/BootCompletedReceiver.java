fc8f560353b52b77be6ff932c87f9eb531246a78
Ahmad Rulim <personal@rulim34.dev>
2023-04-02 11:37:11 +0700

    veux: parts: Update thermal settings UI
    Signed-off-by: Ahmad Rulim <personal@rulim34.dev>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,7 +25,6 @@ import android.util.Log;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
-
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
 
--- parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
+++ parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
@@ -23,10 +23,10 @@ import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 import com.android.settingslib.widget.R;
 
 public class ClearSpeakerActivity extends CollapsingToolbarBaseActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         getFragmentManager().beginTransaction()
                 .replace(R.id.content_frame,  new ClearSpeakerFragment())
                 .commit();
